32bda585ae0af05ab0741c18caa3ba95259a261e
Yason <90974730+famas0078@users.noreply.github.com>
2025-01-14 14:58:54 +0300

    add request range

--- src/services/CTFDataService.js
+++ src/services/CTFDataService.js
@@ -1,10 +1,11 @@
 import http from '../http-common.js'
 export default {
-    getWorkTimeByServiceLife(dateStart, dateEnd, machineClassIds, machineTypeIds) {
+    getWorkTimeByServiceLife(dateStart, dateEnd) {
+        
         return http.get(`/ctf/charts/worktimeByServiceLife`, {
             params: {
-                dateStart: '2000-01-01',
-                dateEnd: '2025-01-01',
+                dateStart: dateStart,
+                dateEnd: dateEnd,
             }
         });
     },
